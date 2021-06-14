bd39582276f721185015ff1130aef087eee7cfd3
unknown <wilyvasquez88@gmail.com>
2021-06-14 18:58:40 -0500

    vista reservas

--- application/views/pagina/detalles_reservas.php
+++ application/views/pagina/detalles_reservas.php
@@ -3,8 +3,9 @@
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <link rel="icon" href="image/xfavicon.png.pagespeed.ic.6C2XCbExx3.webp" type="image/png">
+      <link rel="icon" href="image/xfavicon.png" type="image/png">
       <title>Gran Hotel Huatulco</title>
+      <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
       <link rel="stylesheet" href="<?= base_url()?>assets/css/ByHF1rPRTi.css" />
       <link rel="stylesheet" href="<?= base_url()?>assets/css/A.style.css+responsive.css,Mcc.FaCKucbutY.css.pagespeed.cf.ZSOH81SeOh.css" />
 
@@ -15,6 +16,20 @@
          <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light">
                <a class="navbar-brand logo_h" href="index.html">
+                   <script data-pagespeed-no-defer>//<![CDATA[
+                     (function(){for(var g="function"==typeof Object.defineProperties?Object.defineProperty:function(b,c,a){if(a.get||a.set)throw new TypeError("ES3 does not support getters and setters.");b!=Array.prototype&&b!=Object.prototype&&(b[c]=a.value)},h="undefined"!=typeof window&&window===this?this:"undefined"!=typeof global&&null!=global?global:this,k=["String","prototype","repeat"],l=0;l<k.length-1;l++){var m=k[l];m in h||(h[m]={});h=h[m]}
+                     var n=k[k.length-1],p=h[n],q=p?p:function(b){var c;if(null==this)throw new TypeError("The 'this' value for String.prototype.repeat must not be null or undefined");c=this+"";if(0>b||1342177279<b)throw new RangeError("Invalid count value");b|=0;for(var a="";b;)if(b&1&&(a+=c),b>>>=1)c+=c;return a};q!=p&&null!=q&&g(h,n,{configurable:!0,writable:!0,value:q});var t=this;
+                     function u(b,c){var a=b.split("."),d=t;a[0]in d||!d.execScript||d.execScript("var "+a[0]);for(var e;a.length&&(e=a.shift());)a.length||void 0===c?d[e]?d=d[e]:d=d[e]={}:d[e]=c};function v(b){var c=b.length;if(0<c){for(var a=Array(c),d=0;d<c;d++)a[d]=b[d];return a}return[]};function w(b){var c=window;if(c.addEventListener)c.addEventListener("load",b,!1);else if(c.attachEvent)c.attachEvent("onload",b);else{var a=c.onload;c.onload=function(){b.call(this);a&&a.call(this)}}};var x;function y(b,c,a,d,e){this.h=b;this.j=c;this.l=a;this.f=e;this.g={height:window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,width:window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth};this.i=d;this.b={};this.a=[];this.c={}}
+                     function z(b,c){var a,d,e=c.getAttribute("data-pagespeed-url-hash");if(a=e&&!(e in b.c))if(0>=c.offsetWidth&&0>=c.offsetHeight)a=!1;else{d=c.getBoundingClientRect();var f=document.body;a=d.top+("pageYOffset"in window?window.pageYOffset:(document.documentElement||f.parentNode||f).scrollTop);d=d.left+("pageXOffset"in window?window.pageXOffset:(document.documentElement||f.parentNode||f).scrollLeft);f=a.toString()+","+d;b.b.hasOwnProperty(f)?a=!1:(b.b[f]=!0,a=a<=b.g.height&&d<=b.g.width)}a&&(b.a.push(e),
+                     b.c[e]=!0)}y.prototype.checkImageForCriticality=function(b){b.getBoundingClientRect&&z(this,b)};u("pagespeed.CriticalImages.checkImageForCriticality",function(b){x.checkImageForCriticality(b)});u("pagespeed.CriticalImages.checkCriticalImages",function(){A(x)});
+                     function A(b){b.b={};for(var c=["IMG","INPUT"],a=[],d=0;d<c.length;++d)a=a.concat(v(document.getElementsByTagName(c[d])));if(a.length&&a[0].getBoundingClientRect){for(d=0;c=a[d];++d)z(b,c);a="oh="+b.l;b.f&&(a+="&n="+b.f);if(c=!!b.a.length)for(a+="&ci="+encodeURIComponent(b.a[0]),d=1;d<b.a.length;++d){var e=","+encodeURIComponent(b.a[d]);131072>=a.length+e.length&&(a+=e)}b.i&&(e="&rd="+encodeURIComponent(JSON.stringify(B())),131072>=a.length+e.length&&(a+=e),c=!0);C=a;if(c){d=b.h;b=b.j;var f;if(window.XMLHttpRequest)f=
+                     new XMLHttpRequest;else if(window.ActiveXObject)try{f=new ActiveXObject("Msxml2.XMLHTTP")}catch(r){try{f=new ActiveXObject("Microsoft.XMLHTTP")}catch(D){}}f&&(f.open("POST",d+(-1==d.indexOf("?")?"?":"&")+"url="+encodeURIComponent(b)),f.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),f.send(a))}}}
+                     function B(){var b={},c;c=document.getElementsByTagName("IMG");if(!c.length)return{};var a=c[0];if(!("naturalWidth"in a&&"naturalHeight"in a))return{};for(var d=0;a=c[d];++d){var e=a.getAttribute("data-pagespeed-url-hash");e&&(!(e in b)&&0<a.width&&0<a.height&&0<a.naturalWidth&&0<a.naturalHeight||e in b&&a.width>=b[e].o&&a.height>=b[e].m)&&(b[e]={rw:a.width,rh:a.height,ow:a.naturalWidth,oh:a.naturalHeight})}return b}var C="";u("pagespeed.CriticalImages.getBeaconData",function(){return C});
+                     u("pagespeed.CriticalImages.Run",function(b,c,a,d,e,f){var r=new y(b,c,a,e,f);x=r;d&&w(function(){window.setTimeout(function(){A(r)},0)})});})();
+                     
+                     pagespeed.CriticalImages.Run('/mod_pagespeed_beacon','https://preview.colorlib.com/theme/royal/contact.html','-ilGEe-FWC',true,false,'I2EpgPTK7cY');
+                     //]]>
+                  </script>
                   <img src="<?= base_url('assets/image/logo_hotel.png')?>" width="100px" alt="">
                </a>
                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -74,27 +89,27 @@
                         </div>
                      </div>
                   </div>
-                  <div class="single-post row">
+                  <!-- <div class="single-post row">
                      <img src="<?= base_url()?>assets/image/mastercard.png" style="width:60px" alt="">
                      <img src="<?= base_url()?>assets/image/visa.png" style="width:60px;margin-left: 5px;" alt="">
                      <img src="<?= base_url()?>assets/image/paypal.png" style="width:60px;margin-left: 5px;" alt="">
-                  </div>
+                  </div> -->
                   <div class="comment-form">
                      <h4>Contactanos para mayor informacion</h4>
                      <form>
                         <div class="form-group form-inline">
                            <div class="form-group col-lg-6 col-md-6 name">
-                              <input type="text" class="form-control" id="name" placeholder="Enter Name" >
+                              <input type="text" class="form-control" id="name" placeholder="Nombre" >
                            </div>
                            <div class="form-group col-lg-6 col-md-6 email">
-                              <input type="email" class="form-control" id="email" placeholder="Enter email address">
+                              <input type="email" class="form-control" id="email" placeholder="Direccion de correo">
                            </div>
                         </div>
                         <div class="form-group">
-                           <input type="text" class="form-control" id="subject" placeholder="Subject">
+                           <input type="text" class="form-control" id="subject" placeholder="Asunto">
                         </div>
                         <div class="form-group">
-                           <textarea class="form-control mb-10" rows="5" name="message" placeholder="Messege" required></textarea>
+                           <textarea class="form-control mb-10" rows="5" name="message" placeholder="Mensaje" required></textarea>
                         </div>
                         <a href="#" class="primary-btn button_hover">Enviar</a>
                      </form>
@@ -103,15 +118,12 @@
                <div class="col-lg-4">
                   <div class="blog_right_sidebar">
                      <aside class="single_sidebar_widget author_widget">
-                        <img class="author_img rounded-circle" src="image/blog/xauthor.png.pagespeed.ic.1A1JgX1vWU.webp" alt="">
                         <h4># Reservacion001</h4>
-                        <p>Senior blog writer</p>
-                        <p>Boot camps have its supporters andit sdetractors. Some people do not understand why you should have to spend money on boot camp when you can get. Boot camps have itssuppor ters andits detractors.</p>
+                        <p>Metodos de pago</p><br>
+                        <img src="<?= base_url()?>assets/image/mastercard.png" style="width:60px" alt="">
+                        <img src="<?= base_url()?>assets/image/visa.png" style="width:60px;margin-left: 5px;" alt="">
+                        <img src="<?= base_url()?>assets/image/paypal.png" style="width:60px;margin-left: 5px;" alt="">
                         <div class="br"></div>
-                     </aside>
-                     <aside class="single_sidebar_widget ads_widget">
-                        <a href="#"><img class="img-fluid" src="image/blog/xadd.jpg.pagespeed.ic.cPiWX1WwuT.webp" alt=""></a>
-                        <!-- <div class="br"></div> -->
                      </aside>
                      <aside class="single_sidebar_widget post_category_widget">
                         <h4 class="widget_title">Detalles Reservacion</h4>
@@ -156,7 +168,8 @@
                         <!-- <div class="br"></div> -->
                      </aside>
                      <div class="comment-form" style="padding-top: 10px;padding-bottom:10px;margin-top: 10px;">
-                        <a href="#" class="primary-btn button_hover">PAGAR</a>
+                        <a href="#openModal" class="primary-btn button_hover">PAGAR</a>
+                        <a href="#modal">Launch A Modal</a>
                      </div>
                      <aside class="single-sidebar-widget newsletter_widget">
                         <h4 class="widget_title">Subscribete</h4>
@@ -168,7 +181,7 @@
                               <div class="input-group-prepend">
                                  <div class="input-group-text"><i class="fa fa-envelope" aria-hidden="true"></i></div>
                               </div>
-                              <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Enter email">
+                              <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Escribe tu correo">
                            </div>
                            <a href="#" class="bbtns">Subcribe</a>
                         </div>
@@ -211,7 +224,7 @@
                      <div id="mc_embed_signup">
                         <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" class="subscribe_form relative">
                            <div class="input-group d-flex flex-row">
-                              <input name="EMAIL" placeholder="Direccion de correo" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required type="email">
+                              <input name="EMAIL" placeholder="Direccion de correo" required type="email">
                               <button class="btn sub-btn"><span class="lnr lnr-location"></span></button>
                            </div>
                            <div class="mt-10 info"></div>
@@ -238,7 +251,7 @@
             <div class="border_line"></div>
             <div class="row footer-bottom d-flex justify-content-between align-items-center">
                <p class="col-lg-8 col-sm-12 footer-text m-0">
-                  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved<i class="fa fa-heart-o" aria-hidden="true"></i>
+                  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved <i class="fa fa-heart-o" aria-hidden="true"></i>
                </p>
                <div class="col-lg-4 col-sm-12 footer-social">
                   <a href="#"><i class="fa fa-facebook"></i></a>
@@ -246,7 +259,19 @@
                </div>
             </div>
          </div>
-      </footer>
+      </footer> 
+      <div id="modal">
+        <div class="modal-content">
+          <div class="header">
+            <h2>Modal Header</h2>
+          </div>
+          <div class="copy">
+            <p>Modal Body</p>
+            <a href="#">Close Link</a> </div>
+        </div>
+        <div class="overlay"></div>
+      </div>  
       <script src="<?= base_url()?>assets/js/jquery-3.2.1.min.js"></script>
+      <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
    </body>
 </html>
